7f3df555b479b30a26fd8eec3332d7596af4f6bd
huawt <ghost263sky@163.com>
2024-03-26 17:44:27 +0800

    111111

---
+++ Example/Pods/Target Support Files/LRObjcTools/LRObjcTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LRObjcTools : NSObject
+@end
+@implementation PodsDummy_LRObjcTools
+@end
---
+++ Example/Pods/Target Support Files/Pods-LRObjcTools_Tests/Pods-LRObjcTools_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LRObjcTools_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LRObjcTools_Tests
+@end
